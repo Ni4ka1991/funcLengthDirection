6730d40fed3037522e01c78d3de0f8ca3d4c232f
Ni4ka1991 <astahova_nica@mail.ru>
2021-04-11 23:30:10 +0300

    Add file funcLengthDirection.py

---
+++ funcLengthDirection.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from os import system
+from sys import argv 
+
+
+system( "clear" )
+
+def drawLine( length, direction ):
+    if( direction == "h"):
+     print( "-" * length )
+    elif( direction == "v" ):
+     for i in range( length ):
+      print( "|\n" )
+    else:
+     print( "Be careful! Enter h or v to choose a direction." )
+
+
+drawLine( 7, "h" )
+
+drawLine( 12, "v" )
